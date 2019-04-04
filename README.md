6fb0efb2ca7399bbfcb26c2efa008c28bad6aa70
wk-j <somnuk.wk@gmail.com>
2019-04-04 15:44:51 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## Operation System
+
+[![NuGet](https://img.shields.io/nuget/v/wk.OperatingSystem.svg)](https://www.nuget.org/packages/wk.OperatingSystem)
---
+++ src/OperatingSystem/OS.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace OperatingSystem {
+    public static class OS {
+        public static bool IsWindows() =>
+            RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+
+        public static bool IsMacOS() =>
+            RuntimeInformation.IsOSPlatform(OSPlatform.OSX);
+
+        public static bool IsLinux() =>
+            RuntimeInformation.IsOSPlatform(OSPlatform.Linux);
+    }
+}
